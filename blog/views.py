528a4bd308773ce0bb3f747223e75bca5188a258
HardRope <61661973+HardRope@users.noreply.github.com>
2022-07-10 00:17:21 +0300

    reduce db queries number

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from django.shortcuts import render
 from blog.models import Comment, Post, Tag
 
@@ -15,7 +16,7 @@ def serialize_post_optimized(post):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'tags': [serialize_tag(tag) for tag in post.tags.all().annotate(Count('posts'))],
         'first_tag_title': post.tags.all()[0].title,
     }
 
@@ -37,7 +38,7 @@ def serialize_post(post):
 def serialize_tag(tag):
     return {
         'title': tag.title,
-        'posts_with_tag': Post.objects.filter(tags=tag).count(),
+        'posts_with_tag': tag.posts__count,
     }
 
 
@@ -55,7 +56,7 @@ def index(request):
 
     most_fresh_posts = list(fresh_posts)[-5:]
 
-    most_popular_tags = Tag.objects.popular()[0:5]
+    most_popular_tags = Tag.objects.annotate(Count('posts')).popular()[0:5]
 
     context = {
         'most_popular_posts': [
